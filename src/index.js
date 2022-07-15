8e4f161fcaa832c6537709e0c03c54893ca849a8
Hewsta <s3751537@student.rmit.edu.au>
2022-07-15 23:00:38 +1000

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import App from './App.js'
+
+// components (custom web components)
+import './components/cb-app-header'
+//import './components/cb-product'
+
+
+// styles
+import './scss/master.scss'
+
+// modules
+import { gsap } from "gsap"
+
+
+
+// app.init
+document.addEventListener('DOMContentLoaded', () => {
+  App.init()
+})
+
---
+++ src/views/pages/signup.js
@@ -0,0 +1,91 @@
+import App from '../../App'
+import {html, render } from 'lit-html'
+import {gotoRoute, anchorRoute} from '../../Router'
+import Auth from '../../Auth'
+import Utils from '../../Utils'
+
+class SignUpView {
+  init() {
+    console.log("SignUpView.init");
+    document.title = "Sign Up";
+    this.render();
+    Utils.pageIntroAnim();
+  }
+
+  signUpSubmitHandler(e) {
+    e.preventDefault();
+    const submitBtn = document.querySelector(".submit-btn");
+    submitBtn.setAttribute("loading", "");
+    const formData = e.detail.formData;
+
+    // sign up using Auth
+    Auth.signUp(formData, () => {
+      submitBtn.removeAttribute("loading");
+    });
+  }
+
+  render() {
+    const template = html`
+      <div class="page-content page-centered">
+        <div class="signinup-box">
+          <img class="signinup-logo" src="/images/brandmark.png" />
+          <h1>Sign Up</h1>
+          <sl-form class="form-signup" @sl-submit=${this.signUpSubmitHandler}>
+            <div class="input-group">
+              <sl-input
+                name="firstName"
+                type="text"
+                placeholder="First Name"
+                required
+              ></sl-input>
+            </div>
+            <div class="input-group">
+              <sl-input
+                name="lastName"
+                type="text"
+                placeholder="Last Name"
+                required
+              ></sl-input>
+            </div>
+            <div class="input-group">
+              <sl-input
+                name="email"
+                type="email"
+                placeholder="Email"
+                required
+              ></sl-input>
+            </div>
+            <div class="input-group">
+              <sl-input
+                name="password"
+                type="password"
+                placeholder="Password"
+                required
+                toggle-password
+              ></sl-input>
+            </div>
+            <div class="input-group">
+              <sl-select name="accessLevel" placeholder="I am a ..." required>
+                <sl-menu-item value="1">Worker</sl-menu-item>
+                <sl-menu-item value="2">Seeker</sl-menu-item>
+              </sl-select>
+            </div>
+            <sl-button
+              type="primary"
+              class="submit-btn"
+              submit
+              style="width: 100%;"
+              >Sign Up</sl-button
+            >
+          </sl-form>
+          <p>
+            Have an account? <a href="/signin" @click=${anchorRoute}>Sign In</a>
+          </p>
+        </div>
+      </div>
+    `;
+    render(template, App.rootEl);
+  }
+}
+
+export default new SignUpView();
