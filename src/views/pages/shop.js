bacc86e1e46e1c067de7df17f3d971177a9c3b03
marisarusso <19851569@student.curtin.edu.au>
2022-07-21 18:12:14 +1000

    Update shop.js

--- src/views/pages/shop.js
+++ src/views/pages/shop.js
@@ -17,7 +17,7 @@ class ShopView {
       <div class="shop">
       <div class="page-content">        
         <h1>Shop</h1>
-        <sl-button class="product-btn" type="primary" @click=${() => gotoRoute('/product')}>Product
+        <sl-button class="product-btn" type="primary" @click=${() => gotoRoute('/product')}>PRODUCT
       </sl-button>
         <br>
         <br>
