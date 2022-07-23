7a1df015edf2824ca3c46d770e528e561b733573
marisarusso <19851569@student.curtin.edu.au>
2022-07-23 17:49:03 +1000

    Update src.e31bb0bc.js

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -12956,6 +12956,20 @@ class FavouriteProductsView {
     this.render();
 
     _Utils.default.pageIntroAnim();
+
+    const timeline = gsap.timeline({
+      defaults: {
+        duration: 1
+      }
+    });
+    timeline.from('h1', {
+      opacity: 0
+    }, .2).from('p', {
+      opacity: 0,
+      y: '-50%',
+      ease: 'bounce',
+      stagger: .5
+    }, 1);
   }
 
   render() {
